1bace67f8f114af0c058b0edd40410b7f39af224
Mohammad Shiraz <mohdshiraz980@gmail.com>
2025-02-09 05:07:17 +0530

    deploy

--- server/index.js
+++ server/index.js
@@ -36,7 +36,7 @@ app.use(cookieParser());
 // );
 app.use(
     cors({
-        origin:"http://localhost:3000",
+        origin:"https://study-notion-three-snowy.vercel.app/",
         credentials: true,
     })
 )
